66feedb0aea1d20ab4f19877fd940a5638787b3a
k-oga <gene_selfish_k@yahoo.co.jp>
2014-11-18 18:19:20 +0900

    ちょっとした修正

--- src/chap08/sect01/Main.scala
+++ src/chap08/sect01/Main.scala
@@ -3,9 +3,9 @@ package chap08.sect01
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val x = new EmptySet[Num].incl(Num(1.0)).incl(Num(2.0))
+    val s = new EmptySet[Num].incl(Num(1.0)).incl(Num(2.0))
     println(
-      x.contains(Num(1.5))
+      s.contains(Num(1.5))
     )
   }
 
