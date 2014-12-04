af2081b0624680dbf62e59b2b5d6b5cab99b3ee9
k-oga <gene_selfish_k@yahoo.co.jp>
2014-12-04 18:36:57 +0900

    命令型構造制御

---
+++ src/chap11/sect02/exercise/Exercise.scala
@@ -0,0 +1,23 @@
+package chap11.sect02.exercise
+
+object Exercise {
+
+  def main(args: Array[String]): Unit = {
+    var i = 0
+    repeatLoop {
+      println("i = %d".format(i))
+      i  += 1
+    }(i < 2)
+  }
+
+  /**
+   * 演習 11.2.1 repeatLoop 関数を書きなさい。次のように適用されるものとします。
+   * repeatLoop { command } ( condition )
+   */
+  def repeatLoop(command: => Unit)(condition: => Boolean) {
+    command
+    if (condition) repeatLoop(command)(condition)
+    else ()
+  }
+
+}
